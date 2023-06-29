1a6994b2c0b3f3c762b01d69c9446b94483336ea
Dexter Balderama <dhiee1598@gmail.com>
2023-06-29 20:31:33 +0800

    fixing design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <form>
+        <input type="text" /><br/>
+        <button>ADD</button>
+      </form>
+      <div className="todo-list">
+        <ul>
+          <li>
+            <div className="todo">
+              <input type="checkbox" />
+              <p>Sample Todo</p>
+            </div>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
